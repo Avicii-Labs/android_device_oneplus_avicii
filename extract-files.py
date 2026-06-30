7f7ac2a0142b3a5d1bfb904969a6f3bc932fab4e
ralph950412 <ralph950412@gmail.com>
2026-06-30 03:11:04 +0000

    avicii: Patch libwfdservice to load android.media.audio.common.types-V5-cpp.so
    Change-Id: I4d8b47893f32d28a3bb51c7eccbf52fa72b971d0
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -105,6 +105,8 @@ blob_fixups: blob_fixups_user_type = {
        .apktool_patch('blob-patches/oplus-ims-ext.patch'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=0x01', 'NFC_DEBUG_ENABLED=0x00'),
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
