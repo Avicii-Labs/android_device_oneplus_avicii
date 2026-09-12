3dda7730644056b63b687b33eecf6ebdcc578576
lostsignal-502 <viaan4542@gmail.com>
2026-09-12 10:12:03 +0000

    avicii: Doze: Fix build error from missing wakeUpWithProximityCheck
    PowerManager.wakeUpWithProximityCheck is not a valid API; use the
    standard wakeUp(long, int, String) overload instead.
    
    Signed-off-by: lostsignal-502 <viaan4542@gmail.com>

--- packages/Doze/src/org/lineageos/settings/doze/PickupSensor.kt
+++ packages/Doze/src/org/lineageos/settings/doze/PickupSensor.kt
@@ -13,7 +13,6 @@ import android.hardware.SensorManager
 import android.os.PowerManager
 import android.os.SystemClock
 import android.util.Log
-import android.view.Display
 import java.util.concurrent.Executors
 
 class PickupSensor(
@@ -40,11 +39,10 @@ class PickupSensor(
         if (event.values[0] == sensorValue) {
             if (Utils.isPickUpSetToWake(context)) {
                 wakeLock.acquire(WAKELOCK_TIMEOUT_MS)
-                powerManager.wakeUpWithProximityCheck(
+                powerManager.wakeUp(
                     SystemClock.uptimeMillis(),
                     PowerManager.WAKE_REASON_GESTURE,
                     TAG,
-                    Display.DEFAULT_DISPLAY,
                 )
             } else {
                 Utils.launchDozePulse(context)
